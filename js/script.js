bc0c6e1344a243c016ff56b3b6afbe23e192ddb2
FedericaC03 <fdrk03@hotmail.it>
2020-09-18 16:39:15 +0200

    prima parte dell'esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("");
+$(document).ready(function() {
+
+  var date = "2018-01-01";
+  var momentdate = moment(date);
+  var giorniMese = momentdate.daysInMonth()
+  var mese = momentdate.format("MMMM");
+  console.log(mese);
+//TEMPLATE
+  var source = $("#calendar").html();
+  var template = Handlebars.compile(source);
+
+
+  for (var i = 1; i <= giorniMese; i++) {
+    var context = {
+      "day" : i,
+      "month": mese,
+    };
+    var html = template(context);
+    $(".days").append(html)
+  }
+});
