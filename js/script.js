09e2bac5f709213f6789d212134dc3b2fd89d787
FedericaC03 <fdrk03@hotmail.it>
2020-09-21 13:55:44 +0200

    Esercizio non completato

--- js/script.js
+++ js/script.js
@@ -1,54 +1,116 @@
 $(document).ready(function() {
 
-  var date = "2018-01-01";
+  var date = moment("2018-01-01");
+
+  calendar(date);
+  holidays(date);
+  nextMonth(date);
+  prevMonth(date);
+
+});
+
+function calendar(date) {
+
   var momentdate = moment(date);
   var giorniMese = momentdate.daysInMonth()
   var mese = momentdate.format("MMMM");
-  console.log(mese);
 
-//TEMPLATE
+  $("h1").text(momentdate.format("MMMM " + "YYYY"));
+
+  //TEMPLATE
   var source = $("#calendar").html();
   var template = Handlebars.compile(source);
 
-//CICLO TUTTI I GIORNI DEL MESE E
+  //CICLO TUTTI I GIORNI DEL MESE E
   for (var i = 1; i <= giorniMese; i++) {
+    var newMomentDate = momentdate;
+
     var context = {
       "day" : i,
-      "month": mese,
+      "month": date.format("MMMM"),
       "date" : momentdate.format("YYYY-MM-DD")
     };
     var html = template(context);
     $(".days").append(html);
-    momentdate.add(1, "day");
+    newMomentDate.add(1, "day");
+
+    };
+
   }
-
-//EFFETTUO UNA CHIAMATA AJAX ALL'API DELLE FESTIVITA'
- $.ajax(
-   {
-   "url": "https://flynn.boolean.careers/exercises/api/holidays",
-   "data": {
-     "year":2018,
-     "month": 0
-   },
-     "method": "GET",
-     "success": function(data) {
-        holidays(data.response);
-   },
-   "error": function(errore) {
-     alert("Errore");
-   }
- }
-);
-
-});
 
 //AGGIUNGO AD OGNI FESTIVITA' IL COLORE ROSSO E IL NOME
-function holidays(festivita) {
-  for (var i = 0; i < festivita.length; i++) {
-    var dataFestivita = festivita[i].date;
-    var nomefestivita = festivita[i].name;
+function holidays(date) {
+  var month = date.format("M") - 1;
 
-    $(".day[data-data='"+dataFestivita+"']").addClass("holiday");
-    $(".day[data-data='"+dataFestivita+"'] .holidayName").text( " - " + nomefestivita);
-  }
+  //EFFETTUO UNA CHIAMATA AJAX ALL'API DELLE FESTIVITA'
+  $.ajax(
+    {
+      "url": "https://flynn.boolean.careers/exercises/api/holidays",
+      "data": {
+        "year":2018,
+        "month": month
+      },
+      "method": "GET",
+      "success": function(data) {
+        var festivita = data.response;
+
+        for (var i = 0; i < festivita.length; i++) {
+          var dataFestivita = festivita[i].date;
+          var nomefestivita = festivita[i].name;
+
+          $(".day[data-data='"+dataFestivita+"']").addClass("holiday");
+          $(".day[data-data='"+dataFestivita+"'] .holidayName").text( " - " + nomefestivita);
+        }
+      },
+      "error": function(errore) {
+        alert("Errore");
+      }
+    }
+  );
 }
+
+//FUNZIONE CLICK PREV
+function prevMonth (date) {
+  var nextMonth = $(".prev");
+
+  nextMonth.click(function(){
+    date.subtract(1, "months");
+
+    if (date.year() == 2017) {
+      date.add(1, "months");
+      alert("Spiacente non abbiamo dati disponibili");
+    } else {
+      calendar(date);
+      holidays(date);
+      //TEMPLATE 2
+      // var source = $("#template").html();
+      // var template = Handlebars.compile(source);
+      // $("#mese-corrente").html("");
+      // $("#mese-corrente").append(date.format("MMMM-YYYY"));
+    }
+
+  })
+
+}
+
+//FUNZIONE CLICK NEXT {
+function nextMonth (date) {
+  var nextMonth = $(".next");
+
+  nextMonth.click(function(){
+    date.add(1, "months");
+
+    if (date.year() == 2019) {
+      date.subtract(1, "months");
+      alert("Spiacente non abbiamo dati disponibili");
+    } else {
+      calendar(date);
+      holidays(date);
+      //TEMPLATE 2
+      // var source = $("#template").html();
+      // var template = Handlebars.compile(source);
+      // $("#mese-corrente").html("");
+      // $("#mese-corrente").append(date.format("MMMM-YYYY"));
+    }
+  })
+};
